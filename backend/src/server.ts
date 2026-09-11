bb9f6d80e17267c20d025b645be415c2c2e1648b
Deepak Aggarwal <deepakagg1206@gmail.com>
2026-09-11 13:59:49 +0530

    Changed the frontend url in the node backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors({
-    origin: "http://localhost:3001",
+    origin: process.env.FRONTEND_URL,
     credentials: true
 }));
 app.use(express.json());
